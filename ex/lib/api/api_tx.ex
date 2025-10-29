8f60ef69f6f1ba4111faff126907bd58b7d76eec
GerardMensoif <gerard.mensoif6666@gmail.com>
2025-10-29 19:35:51 +0100

    Add files via upload

--- ex/lib/api/api_tx.ex
+++ ex/lib/api/api_tx.ex
@@ -186,6 +186,11 @@ defmodule API.TX do
             Map.put(a, :args, args)
         end)
         tx = put_in(tx, [:tx, :actions], actions)
-        tx = put_in(tx, [:metadata, :entry_hash], Base58.encode(tx.metadata.entry_hash))
+        # Encoder metadata seulement si elle existe
+        tx = if Map.has_key?(tx, :metadata) do
+            put_in(tx, [:metadata, :entry_hash], Base58.encode(tx.metadata.entry_hash))
+        else
+            tx
+        end
     end
 end
